ae3086f4c1ac23c0f794da1301907eb6b145b9df
Chaslinux <97259120+chaslinux@users.noreply.github.com>
2026-03-23 09:01:42 -0400

    Update wificheck.sh
    Add case insensitivity to wifi and bluetooth checks as some machines use capitals, and others do not.

--- wificheck.sh
+++ wificheck.sh
@@ -3,12 +3,12 @@
 BTVERSION=$(hciconfig -a | grep "LMP Version:" | cut -c 15- | cut -c -3)
 sudo apt install rfkill -y
 
-if sudo iw dev | grep -q Interface; then
+if sudo iw dev | grep -qi Interface; then
     echo "Wifi present"
 else
     echo "No Wifi"
 fi
-if sudo rfkill list | grep -q bluetooth; then
+if sudo rfkill list | grep -qi bluetooth; then
     echo "Bluetooth $BTVERSION"
 else
     echo "No Bluetooth"
